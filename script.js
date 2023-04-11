d5cd8e013b91169d8d1c6c3bba22131fa03efa32
Oleg Pyzh <pyzh1oleg@gmail.com>
2023-04-11 23:42:08 +0300

    Working function, no comments in code

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+// An object with 2 nested objects: tips (arrays with motivational tips) and blocks (phrases for completing the tips). The script works only if tips and blocks contain the same number of arrays.
 const tipsObj = {
-    tips: {
-        arr1: ['lose control over your emotions', 'get lost in negative thoughts', 'please laziness', 'let bad habits ruin your life'],
-        arr2: ['Believe in yourself and all that you are', 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'The future belongs to those who believe in the beauty of their dreams', 'You are never too old to set another goal or to dream a new dream', 'Do not wait for opportunities, create them', 'You miss 100% of the shots you don\'t take', 'Don\'t be afraid to fail. Be afraid not to try'],
-        arr3: ['Persistence', 'Gratitude', 'Consistency', 'Action', 'Courage', 'Discipline', 'Resilience', 'Positivity', 'Patience', 'Focus', 'Kindness', 'Self-belief', 'Perseverance', 'Growth mindset', 'Mindfulness']
-    },
-    blocks: {
-        arr1: ['Do\'t', 'You should never', 'Try not to', 'Never'],
-        arr2: ['Reminder:', 'Keep in mind:', 'Important:'],
-        arr3: ['is your rule of thumb', 'is what you need to practice', 'is what you need', 'is in your character', 'is your good feature']
+    arr1: [['Don\'t', 'You should never', 'Try not to', 'Never'], ['lose control over your emotions', 'get lost in negative thoughts', 'please laziness', 'let bad habits ruin your life']],
+    arr2: [['Reminder:', 'Keep in mind:', 'Important:'], ['Believe in yourself and all that you are', 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'The future belongs to those who believe in the beauty of their dreams', 'You are never too old to set another goal or to dream a new dream', 'Do not wait for opportunities, create them', 'You miss 100% of the shots you don\'t take', 'Don\'t be afraid to fail. Be afraid not to try']],
+    arr3: [['Persistence', 'Gratitude', 'Consistency', 'Action', 'Courage', 'Discipline', 'Resilience', 'Positivity', 'Patience', 'Focus', 'Kindness', 'Self-belief', 'Perseverance', 'Growth mindset', 'Mindfulness'], ['is your rule of thumb', 'is what you need to practice', 'is what you need', 'is in your character', 'is your good feature']]
+}
+
+let mixedMessage = obj => {
+    // arrays where we will push our random tips and random blocks
+    let tipsList = [];
+    for (let key in obj) {
+        let tip = [];
+        for (let i = 0; i < obj[key].length; i++) {
+            let randTip = obj[key][i][Math.floor(Math.random() * obj[key][i].length)];
+            tip.push(randTip);
+        }
+        tipsList.push(tip.join(' '));
+    }
+    console.log('Motivational tips of the day:')
+    const temp = tipsList.length
+    for (let i = 0; i < temp; i++) {
+        let tip = tipsList.splice(Math.floor(Math.random() * tipsList.length), 1);
+        console.log(tip.toString());
     }
 }
+
+mixedMessage(tipsObj);
